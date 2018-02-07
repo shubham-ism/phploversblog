9ccd8e129475261c4f255707c89694e2b307760a
shubham-ism <shubham.gup1811@gmail.com>
2018-02-07 21:22:07 +0530

    files added

---
+++ admin/edit_category.php
@@ -0,0 +1,29 @@
+<?php include'includes/header.php'?>
+<?php 
+
+	$id = $_GET['id'];
+	//Create DB Object
+	$db = new Database();
+	
+	//Create Query
+	$query = "SELECT * FROM categories 
+				WHERE id=".$id;
+	
+	//Run Query
+	$category = $db->select($query)->fetch_assoc();
+
+?>
+<form role="form" method="post" action="edit_category.php">
+  <div class="form-group">
+    <label>Category Name</label>
+    <input name="name" type="text" class="form-control" placeholder="Enter Category" value="<?php echo $category['name'];?>">
+  </div>
+  <div>
+    <input name="submit" type="submit" class="btn btn-default" value="Submit"/> 
+    <a href="index.php" class="btn btn-default">Cancel</a>
+    <input name="delete" type="submit" class="btn btn-danger" value="Delete"/>
+  </div>
+  <br>
+</form>
+
+<?php include'includes/footer.php'?>
